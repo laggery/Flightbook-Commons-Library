4d78be283736c4a7346a6e3ba5021faae2bd3079
fabiangotzen <fabian.gotzen@gmail.com>
2021-05-27 15:35:42 -0600

    fix typo

--- projects/flightbook-commons/src/lib/fileupload/fileupload.service.ts
+++ projects/flightbook-commons/src/lib/fileupload/fileupload.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,12 @@ export class FileUploadService {
   constructor(private httpClient: HttpClient, @Inject('env') private environment) {
   }
 
-  uploadFile(formData: FormData, userId: string): Observable<any> {
+  uploadFile(formData: FormData): Observable<any> {
     return this.httpClient
-      .post(`${this.environment.baseUrl}/upload/igcfile/staging/${userId}`, formData);
+      .post(`${this.environment.baseUrl}/upload/igcfile`, formData)
+      .pipe(
+        map((response: any) => {
+        })
+      );
   }
 }
